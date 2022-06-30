b48491f1d9cf8a07300bfbe55752250fd32bf2ea
aprxty3 <ajiprxty3@gmail.com>
2022-06-30 21:02:59 +0700

    first init

---
+++ bin/relearn_dart_with_pzn.dart
@@ -0,0 +1,5 @@
+import 'package:relearn_dart_with_pzn/relearn_dart_with_pzn.dart' as relearn_dart_with_pzn;
+
+void main(List<String> arguments) {
+  print('Hello world: ${relearn_dart_with_pzn.calculate()}!');
+}
---
+++ test/relearn_dart_with_pzn_test.dart
@@ -0,0 +1,8 @@
+import 'package:relearn_dart_with_pzn/relearn_dart_with_pzn.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
