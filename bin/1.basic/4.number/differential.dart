990bb54d23aff464109786c5663cc16325004128
aprxty3 <ajiprxty3@gmail.com>
2022-06-30 22:49:39 +0700

    finish course about number

---
+++ bin/1.basic/4.number/differential.dart
@@ -0,0 +1,23 @@
+void main() {
+  int number1 = 12;
+  double number2 = 1.2;
+  num number3 = 10;
+
+  print(number1);
+  number1 = 13;
+  print(number1);
+  number1 = 13.1;
+  print(number1);
+  print('');
+  print(number2);
+  number2 = 14.2;
+  print(number2);
+  number2 = 12;
+  print(number2);
+  print('');
+  print(number3);
+  number3 = 11.2;
+  print(number3);
+  number3 = 12;
+  print(number3);
+}
---
+++ bin/1.basic/4.number/int_double.dart
@@ -0,0 +1,7 @@
+void main() {
+  int number1 = 12;
+  double number2 = 1.2;
+
+  print(number1);
+  print(number2);
+}
