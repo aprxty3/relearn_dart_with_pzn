2ee2bb9c3a8752e67db28d8c8c8bb633f47d8d48
aprxty3 <ajiprxty3@gmail.com>
2022-07-01 06:56:00 +0700

    finish course about aritmatic

---
+++ bin/1.basic/9.aritmatic_operation/example.dart
@@ -0,0 +1,19 @@
+void main() {
+  var res1 = 10 + 10;
+  var res2 = 10 - 8;
+  var res3 = -19 + 10;
+  var res4 = -19 - -12;
+  var res5 = 10 * 10;
+  var res6 = 7 / 2;
+  var res7 = 9 ~/ 4;
+  var res8 = 17 % 3;
+
+  print(res1);
+  print(res2);
+  print(res3);
+  print(res4);
+  print(res5);
+  print(res6);
+  print(res7);
+  print(res8);
+}
