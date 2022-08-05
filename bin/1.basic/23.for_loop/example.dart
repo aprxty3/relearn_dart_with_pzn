1dc843a8453d78ab3327e810a77c9f4fa3d04a05
aprxty3 <ajiprxty3@gmail.com>
2022-08-05 21:48:46 +0700

    ekse

---
+++ bin/1.basic/23.for_loop/example.dart
@@ -0,0 +1,19 @@
+void main() {
+  //TODO infinite loop
+  // for (;;) {
+  //   print('infinite loop');
+  // }
+
+  //TODO kondisi
+  // var counter = 1;
+  // for (; counter <= 10;) {
+  //   print('loop ke- ' + counter.toString());
+  //
+  //   counter++;
+  // }
+
+  //TODO next kondisi
+  for (var counter = 1; counter <= 10; counter++) {
+    print('loop ke- $counter');
+  }
+}
