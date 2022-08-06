ea54996395613ba0940fdd8620387fc36ede4d1e
aprxty3 <ajiprxty3@gmail.com>
2022-08-06 18:38:44 +0700

    ekse

--- bin/1.basic/32.function_retun_value/example.dart
+++ bin/1.basic/32.function_retun_value/example.dart
@@ -1,3 +1,19 @@
-void main(){
+void main() {
+  var data = sayHello('aji');
+  print(data);
 
+  var total = sum([50, 50, 50, 50, 50]);
+  print(total);
+}
+
+String sayHello(String name) {
+  return 'haya $name';
+}
+
+int sum(List<int> numbers) {
+  var total = 0;
+  for (var value in numbers) {
+    total += value;
+  }
+  return total;
 }
--- bin/1.basic/33.function_short_expression/example.dart
+++ bin/1.basic/33.function_short_expression/example.dart
@@ -1,3 +1,5 @@
-void main(){
-
+void main() {
+  print(sum(10, 20));
 }
+
+int sum(int first, int second) => first + second;
