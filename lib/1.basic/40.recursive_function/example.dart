be8f69376acfa7ddb7fb13ebfae5433005f7073b
aprxty3 <ajiprxty3@gmail.com>
2022-08-06 21:16:15 +0700

    refactor

--- lib/1.basic/40.recursive_function/example.dart
+++ lib/1.basic/40.recursive_function/example.dart
@@ -1,3 +1,35 @@
-void main(){
+void main() {
+  print(factorialLoop(4));
+  print(factorialRecursive(4));
 
+  loop(100000);
+}
+
+//TODO : factorial tanpa recursive
+int factorialLoop(int value) {
+  var result = 1;
+  for (var i = 1; i <= value; i++) {
+    result *= i;
+  }
+
+  return result;
+}
+
+//TODO : factorial dengan recursive
+int factorialRecursive(int value) {
+  if (value == 1) {
+    return 1;
+  } else {
+    return value * factorialRecursive(value - 1);
+  }
+}
+
+//TODO : stack overflow error
+void loop(int value) {
+  if (value == 0) {
+    print('Selesai');
+  } else {
+    print('perulangan ke $value');
+    loop(value - 1);
+  }
 }
