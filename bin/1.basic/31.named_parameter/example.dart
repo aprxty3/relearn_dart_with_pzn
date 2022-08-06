bce0b9c9e482d59beea98e6b441e1155c734db03
aprxty3 <ajiprxty3@gmail.com>
2022-08-06 16:54:53 +0700

    ekse

--- bin/1.basic/31.named_parameter/example.dart
+++ bin/1.basic/31.named_parameter/example.dart
@@ -1,3 +1,28 @@
-void main(){
+void main() {
+  sayHellp();
+  sayHellp(firstName: 'om', lastName: 'aji');
+  sayHellp(lastName: 'om');
+  sayHellp(lastName: 'ooom', firstName: 'ajjii');
 
+  print('------------');
+  defaultValue();
+  defaultValue(firstName: 'aji');
+  defaultValue(firstName: 'om', lastName: 'aji');
+
+  print('--------------');
+  required(firstName: 'om', lastName: 'aji');
+}
+
+void sayHellp({String? firstName, String? lastName}) {
+  print('hello $firstName $lastName');
+}
+
+//TODO DefaultValue
+void defaultValue({String firstName = '', String lastName = 'pras'}) {
+  print('hello $firstName $lastName');
+}
+
+//TODO menggunakan Required = diharuskan untuk diisi
+void required({required String firstName, required String lastName}) {
+  print('hay $firstName $lastName');
 }
