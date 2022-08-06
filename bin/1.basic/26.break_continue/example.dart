aa76fb7ededd237eebf096e3ea45951961052d75
aprxty3 <ajiprxty3@gmail.com>
2022-08-06 16:15:28 +0700

    ekse

--- bin/1.basic/26.break_continue/example.dart
+++ bin/1.basic/26.break_continue/example.dart
@@ -1,3 +1,22 @@
-void main(){
+//TODO break
+// void main() {
+//   var counter = 1;
+//
+//   while (true) {
+//     print('perulangan ke- $counter');
+//     counter++;
+//     if (counter > 10) {
+//       break;
+//     }
+//   }
+// }
 
+//TODO continue
+void main() {
+  for (var counter = 1; counter <= 100; counter++) {
+    if (counter % 2 == 0) {
+      continue;
+    }
+    print('perulangan ke-$counter');
+  }
 }
--- bin/1.basic/27.for_in/example.dart
+++ bin/1.basic/27.for_in/example.dart
@@ -1,3 +1,21 @@
-void main(){
+//TODO tanpa for in
+// void main() {
+//   var name = <String>['aji', 'pras', 'dwi'];
+//   for (var i = 0; i < name.length; i++) {
+//     print(name[i]);
+//   }
+// }
 
+//TODO dengan for in
+void main() {
+  var name = ['aji', 'pras', 'dwi'];
+  var nameSet = {'aji', 'pras', 'dwi'};
+
+  for (var value in name) {
+    print(value);
+  }
+
+  for (var values in nameSet) {
+    print(values);
+  }
 }
