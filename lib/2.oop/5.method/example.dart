231549d94b19def0e2a6e3ec19538c23ee2402b3
aprxty3 <ajiprxty3@gmail.com>
2022-08-07 23:35:45 +0700

    ekse

--- lib/2.oop/5.method/example.dart
+++ lib/2.oop/5.method/example.dart
@@ -1,1 +1,28 @@
-void main() {}
+void main() {
+  var person1 = Person();
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
+
+  //TODO manipulasi field
+  var person = Person();
+  person.name = 'aji aji';
+  person.address = 'jalan jalan';
+  // person.country = 'bali'; //Error karena Final
+
+  print('${person.name} ${person.address}');
+  person.sayHello('om firman');
+
+  Person person2 = Person();
+  print(person2);
+}
+
+class Person {
+  String name = 'Guest';
+  String? address;
+  final String country = "Indonesia";
+
+  void sayHello(String paraName) {
+    print('Hello $paraName , my name is $name');
+  }
+}
