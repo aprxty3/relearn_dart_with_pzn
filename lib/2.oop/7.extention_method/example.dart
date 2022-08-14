34058f892db03f0e39d000c0bf7b7dee69cdad64
aprxty3 <ajiprxty3@gmail.com>
2022-08-14 23:25:24 +0700

    ekse

--- lib/2.oop/7.extention_method/example.dart
+++ lib/2.oop/7.extention_method/example.dart
@@ -1,1 +1,35 @@
-void main(){}
+void main() {
+  var person1 = Person();
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
+
+  //TODO manipulasi field
+  var person = Person();
+  person.name = 'aji aji';
+  person.address = 'jalan jalan';
+  // person.country = 'bali'; //Error karena Final
+
+  print('${person.name} ${person.address}');
+  person.sayHello('om firman');
+
+  Person person2 = Person();
+  print(person2);
+  person1.sayGoodBye('joko');
+}
+
+class Person {
+  String name = 'Guest';
+  String? address;
+  final String country = "Indonesia";
+
+  void sayHello(String paraName) {
+    print('Hello $paraName , my name is $name');
+  }
+}
+
+extension GoodByeOnPerson on Person {
+  void sayGoodBye(String paramName) {
+    print('Good Bye $paramName, from $name');
+  }
+}
