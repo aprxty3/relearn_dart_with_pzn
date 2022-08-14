36fa935408001c234c18715665ee14d2d0644a83
aprxty3 <ajiprxty3@gmail.com>
2022-08-14 23:19:00 +0700

    ekse

--- lib/2.oop/6.method_expression_body/example.dart
+++ lib/2.oop/6.method_expression_body/example.dart
@@ -1,1 +1,14 @@
-void main(){}
+void main() {
+  var computer = Computer();
+  computer.startup();
+  computer.shutdown();
+  print(computer.getOperatigSystem());
+}
+
+class Computer {
+  void startup() => print('Computer is Startig');
+
+  void shutdown() => print('Computer is Shutting Down');
+
+  String getOperatigSystem() => 'hahahaha';
+}
