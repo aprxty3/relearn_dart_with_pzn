fd9583e005a7af97110c897cea23d6653b65667c
aprxty3 <ajiprxty3@gmail.com>
2022-08-14 23:36:19 +0700

    ekse

--- lib/2.oop/8.operator/example.dart
+++ lib/2.oop/8.operator/example.dart
@@ -1,1 +1,20 @@
-void main(){}
+void main() {
+  var orange1 = Orange();
+  orange1.quantity = 10;
+
+  var orang2 = Orange();
+  orang2.quantity = 9;
+
+  var orange3 = orange1 + orang2;
+  print(orange3.quantity);
+}
+
+class Orange {
+  int quantity = 0;
+
+  Orange operator +(Orange orange) {
+    var result = Orange();
+    result.quantity = quantity * quantity;
+    return result;
+  }
+}
