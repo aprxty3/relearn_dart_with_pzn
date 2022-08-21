f61a2e3c2f8d67b905d31394b5fc2fbe14cbbfb8
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 13:02:34 +0700

    ekse

--- lib/2.oop/9.constructor/example.dart
+++ lib/2.oop/9.constructor/example.dart
@@ -1,1 +1,17 @@
-void main(){}
+void main() {
+  var person = Person('aji', 'yogya');
+
+  print(person.name);
+  print(person.address);
+}
+
+class Person {
+  String name = 'Guest';
+  String? address;
+  final String country = "Indonesia";
+
+  Person(String paramName, String paramAddress) {
+    name = paramName;
+    address = paramAddress;
+  }
+}
