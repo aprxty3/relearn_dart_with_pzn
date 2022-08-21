34305658d15cfc1c511c239057afd1a5ef0fb242
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 16:31:56 +0700

    ekse

--- lib/2.oop/23.super_constructor/example.dart
+++ lib/2.oop/23.super_constructor/example.dart
@@ -1,1 +1,19 @@
-void main(){}
+void main() {
+  var manager = Manager('ajjii');
+  print(manager.name);
+
+  var myName = MyNames('ajjjjjiii');
+  print(myName.name);
+}
+
+class Manager {
+  String? name;
+
+  Manager(this.name);
+}
+
+class MyNames extends Manager {
+  MyNames(String name) : super(name) {
+    print('Create New Name');
+  }
+}
