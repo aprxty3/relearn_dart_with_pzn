ea28faf4c8d2ab1d3e931fad1717f83f7ba46c24
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 14:26:26 +0700

    ekse

--- lib/2.oop/16.constant_constructor/example.dart
+++ lib/2.oop/16.constant_constructor/example.dart
@@ -1,1 +1,17 @@
-void main(){}
+void main() {
+  var point1 = ImmutablePoint(10, 10);
+  var point2 = ImmutablePoint(10, 10);
+
+  var point3 = const ImmutablePoint(10, 10);
+  var point4 = const ImmutablePoint(10, 10);
+
+  print(point1 == point2);
+  print(point3 == point4);
+}
+
+class ImmutablePoint {
+  final int x;
+  final int y;
+
+  const ImmutablePoint(this.x, this.y);
+}
