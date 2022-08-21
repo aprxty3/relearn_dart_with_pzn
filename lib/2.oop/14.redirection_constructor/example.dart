166ab2a1ac7f3b5a4130416427b79ee505256fbd
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 13:54:47 +0700

    ekse

--- lib/2.oop/14.redirection_constructor/example.dart
+++ lib/2.oop/14.redirection_constructor/example.dart
@@ -1,1 +1,33 @@
-void main(){}
+void main() {
+  var person = Person('aji', 'yogya');
+  var person1 = Person.withName('aji pras');
+  var person2 = Person.withAdress('Slawi');
+  var person3 = Person.fromSlawi();
+
+  print(person.name);
+  print(person.address);
+  print('--------------');
+  print(person1.name);
+  print(person1.address);
+  print('--------------');
+  print(person2.name);
+  print(person2.address);
+
+  print('--------------');
+  print(person3.name);
+  print(person3.address);
+}
+
+class Person {
+  String name = 'Guest';
+  String? address;
+  final String country = "Indonesia";
+
+  Person(this.name, this.address);
+
+  Person.withName(String name) : this(name, 'no adress');
+
+  Person.withAdress(String address) : this('no name', address);
+
+  Person.fromSlawi() : this.withAdress('Jakarta');
+}
