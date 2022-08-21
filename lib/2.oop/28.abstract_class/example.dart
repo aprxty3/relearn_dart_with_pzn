98a2af92b07e7a2c1936f8f5e3f4678dca53ffea
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 17:23:27 +0700

    ekse

--- lib/2.oop/28.abstract_class/example.dart
+++ lib/2.oop/28.abstract_class/example.dart
@@ -1,1 +1,18 @@
-void main() {}
+void main() {
+
+  // var location = Location(); // Gabisa, karena classnya Abstract
+
+  var city = City('Manchester');
+  print(city.name);
+}
+
+//TODO abstract class digunakan sebagai parent class
+abstract class Location {
+  String? name;
+}
+
+class City extends Location {
+  City(String name) {
+    this.name = name;
+  }
+}
