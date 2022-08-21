4a473679d727b4203832ab838df2051df9520dcf
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 16:19:24 +0700

    ekse

--- lib/2.oop/21.field_overriding/example.dart
+++ lib/2.oop/21.field_overriding/example.dart
@@ -1,1 +1,21 @@
-void main(){}
+void main() {
+  var person = Person();
+  person.sayHello('aji');
+
+  print('------------');
+
+  var person1 = OtherPerson();
+  person1.sayHello('ajiiii');
+}
+
+class Person {
+  String name = 'Person';
+
+  void sayHello(String name) {
+    print('hi $name, my name is ${this.name}');
+  }
+}
+
+class OtherPerson extends Person {
+  String name = 'Other Person';
+}
