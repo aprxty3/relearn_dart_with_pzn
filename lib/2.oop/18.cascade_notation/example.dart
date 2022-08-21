408db0e51f5a846427089cf3f513b09e405d33df
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 14:56:24 +0700

    ekse

--- lib/2.oop/18.cascade_notation/example.dart
+++ lib/2.oop/18.cascade_notation/example.dart
@@ -1,1 +1,29 @@
-void main(){}
+void main() {
+//TODO tidak menggunakan cascade notation
+  var user = User();
+  user.username = 'aji';
+  user.name = 'ajipras';
+  user.mail = 'ajipras.com';
+
+  //TODO menggunakan cascade notation
+  var user1 = User()
+    ..username = 'aji'
+    ..name = 'ajipras'
+    ..mail = 'ajipras.com';
+
+  //TODO Nullable cascade notation
+  User? user2 = createUser()
+    ?..username = 'aji'
+    ..name = 'ajipras'
+    ..mail = 'ajipras.com';
+}
+
+class User {
+  String? username;
+  String? name;
+  String? mail;
+}
+
+User? createUser() {
+  return null;
+}
