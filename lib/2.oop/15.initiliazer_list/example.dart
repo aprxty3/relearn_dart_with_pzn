9fd890b1b851205f0e4e8d7698ee02ca3e15b11e
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 14:01:35 +0700

    ekse

--- lib/2.oop/15.initiliazer_list/example.dart
+++ lib/2.oop/15.initiliazer_list/example.dart
@@ -1,1 +1,19 @@
-void main(){}
+void main() {
+  var customer = Customer('Aji Pras');
+
+  print(customer.fullName);
+  print(customer.firstName);
+  print(customer.lastName);
+}
+
+class Customer {
+  String firstName = '';
+  String lastName = '';
+  String fullName = '';
+
+  Customer(this.fullName)
+      : firstName = fullName.split(" ")[0],
+        lastName = fullName.split(' ')[1] {
+    print('Creat new Customer');
+  }
+}
