4b20fb745b5a2427a0e15b7296c5cde6fcaf0ed1
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 15:13:02 +0700

    ekse

--- lib/2.oop/19.inheritance/example.dart
+++ lib/2.oop/19.inheritance/example.dart
@@ -1,1 +1,21 @@
-void main(){}
+void main() {
+  var manager = Manager();
+  manager.name = 'aji';
+  manager.sayHello('aaajii');
+
+  print('-----------');
+
+  var myName = MyName();
+  myName.name = 'asd';
+  myName.sayHello('asdadf');
+}
+
+class Manager {
+  String? name;
+
+  void sayHello(String name) {
+    print('Hello $name, my name is ${this.name}');
+  }
+}
+
+class MyName extends Manager {}
