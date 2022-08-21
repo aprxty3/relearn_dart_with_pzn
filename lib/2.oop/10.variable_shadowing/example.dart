fca3835812f5bff2d64ddd03ae7c30a3036ef6de
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 13:06:07 +0700

    ekse

--- lib/2.oop/10.variable_shadowing/example.dart
+++ lib/2.oop/10.variable_shadowing/example.dart
@@ -1,1 +1,17 @@
-void main(){}
+void main() {
+  var person = Person('aji', 'yogya');
+
+  print(person.name);
+  print(person.address);
+}
+
+class Person {
+  String name = 'Guest';
+  String? address;
+  final String country = "Indonesia";
+
+  Person(String name, String address) {
+    name = name; //Field name tidak berubah
+    address = address; //Field adress tidak berubah
+  }
+}
