a9c826e41ed503178345098dc59c9f1a484568b1
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 17:10:25 +0700

    ekse

--- lib/2.oop/26.type_check_cast/example.dart
+++ lib/2.oop/26.type_check_cast/example.dart
@@ -1,1 +1,42 @@
-void main() {}
+import 'package:relearn_dart_with_pzn/2.oop/25.polymorphism/example.dart';
+
+void main() {
+  Emplyee emplyee = Emplyee('aajiii');
+  print(emplyee);
+
+  emplyee = Manager('ajii');
+  print(emplyee);
+
+  emplyee = VicePres('aaaaajiii');
+  print(emplyee);
+
+  sayHello(Emplyee('aajii'));
+  sayHello(Manager('aajiiaa'));
+  sayHello(VicePres('aasdajii'));
+}
+
+class Emplyee {
+  String name;
+
+  Emplyee(this.name);
+}
+
+class Manager extends Emplyee {
+  Manager(String name) : super(name);
+}
+
+class VicePres extends Manager {
+  VicePres(String name) : super(name);
+}
+
+void sayHello(Emplyee emplyee) {
+  if (emplyee is VicePres) {
+    VicePres vicePres = emplyee as VicePres;
+    print('Hello VP ${vicePres.name}');
+  } else if (emplyee is Manager) {
+    Manager manager = emplyee as Manager;
+    print('Hello Manager ${manager.name}');
+  } else {
+    print('Hello ${emplyee.name}');
+  }
+}
