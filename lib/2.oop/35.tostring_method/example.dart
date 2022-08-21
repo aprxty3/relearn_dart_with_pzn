86cdb2b4b21847f05bc9ed7b37253210842e7668
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 22:28:55 +0700

    ekse

--- lib/2.oop/35.tostring_method/example.dart
+++ lib/2.oop/35.tostring_method/example.dart
@@ -1,1 +1,10 @@
-void main() {}
+import 'package:relearn_dart_with_pzn/2.oop/35.tostring_method/data.dart';
+
+void main() {
+  var product = Product();
+  product.id = '1';
+  product.name = 'haha';
+
+  print(product.toString());
+  print(product);
+}
