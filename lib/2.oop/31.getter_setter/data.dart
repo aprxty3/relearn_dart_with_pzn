a3dc0b39629253e97a24d501ebc3125b2e79ece2
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 20:45:15 +0700

    ekse

---
+++ lib/2.oop/31.getter_setter/data.dart
@@ -0,0 +1,20 @@
+class Rectangle {
+  int _width = 0;
+  int _lenght = 0;
+
+  int get width {
+    return _width;
+  }
+
+  set width(int value) {
+    _width = value;
+  }
+
+  int get lenght {
+    return _lenght;
+  }
+
+  set lenght(int value) {
+    _lenght = value;
+  }
+}
