9b286a389b507c021d53c7f680a39868f3477342
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 16:24:50 +0700

    ekse

--- lib/2.oop/22.super_keyword/example.dart
+++ lib/2.oop/22.super_keyword/example.dart
@@ -1,1 +1,21 @@
-void main(){}
+void main() {
+  var rec = Rectangle();
+  print(rec.getCorner());
+  print(rec.getCornerFromParent());
+}
+
+class Shape {
+  int getCorner() {
+    return 0;
+  }
+}
+
+class Rectangle extends Shape {
+  int getCorner() {
+    return 4;
+  }
+
+  int getCornerFromParent() {
+    return super.getCorner();
+  }
+}
