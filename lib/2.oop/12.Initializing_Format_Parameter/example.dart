27e2f93c1fc42a6fc11211073d17b40a3d462ae4
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 13:13:28 +0700

    ekse

--- lib/2.oop/12.Initializing_Format_Parameter/example.dart
+++ lib/2.oop/12.Initializing_Format_Parameter/example.dart
@@ -1,1 +1,14 @@
-void main(){}
+void main() {
+  var person = Person('aji', 'yogya');
+
+  print(person.name);
+  print(person.address);
+}
+
+class Person {
+  String name = 'Guest';
+  String? address;
+  final String country = "Indonesia";
+
+  Person(this.name, this.address) {}
+}
