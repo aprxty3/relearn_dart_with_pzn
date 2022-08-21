d80b61a89920756dd3560d10061a15f001aa34b9
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 20:55:31 +0700

    ekse

--- lib/2.oop/31.getter_setter/data.dart
+++ lib/2.oop/31.getter_setter/data.dart
@@ -1,20 +1,47 @@
 class Rectangle {
-  int _width = 0;
-  int _lenght = 0;
+  int _width = 1;
+  int _lenght = 1;
 
-  int get width {
-    return _width;
-  }
+  //TODO Default
+  // int get width {
+  //   return _width;
+  // }
+  //
+  // set width(int value) {
+  //   _width = value;
+  // }
+  //
+  // int get lenght {
+  //   return _lenght;
+  // }
+  //
+  // set lenght(int value) {
+  //   _lenght = value;
+  // }
+
+  //TODO menggunakan arrow function
+  // int get width => _width;
+  //
+  // set width(int value) => _width = value;
+  //
+  // int get lenght => _lenght;
+  //
+  // set lenght(int value) => _lenght = value;
+
+  //TODO validation getter setter
+  int get width => _width;
 
   set width(int value) {
-    _width = value;
+    if (value >= 1) {
+      _width = value;
+    }
   }
 
-  int get lenght {
-    return _lenght;
-  }
+  int get lenght => _lenght;
 
   set lenght(int value) {
-    _lenght = value;
+    if (value <= 1) {
+      _lenght = value;
+    }
   }
 }
--- lib/2.oop/31.getter_setter/example.dart
+++ lib/2.oop/31.getter_setter/example.dart
@@ -1,2 +1,10 @@
-void main() {}
+import 'package:relearn_dart_with_pzn/2.oop/31.getter_setter/data.dart';
 
+void main() {
+  var rectangle = Rectangle();
+  rectangle.width = 100;
+  print(rectangle.width);
+
+  rectangle.lenght = 200;
+  print(rectangle.lenght);
+}
