7c731e7486885c0fa35a484448f418c74d6c13d2
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 17:02:59 +0700

    ekse

--- lib/2.oop/25.polymorphism/example.dart
+++ lib/2.oop/25.polymorphism/example.dart
@@ -1,1 +1,34 @@
-void main() {}
+void main() {
+  Emplyee emplyee = Emplyee('aajiii');
+  print(emplyee);
+
+  emplyee = Manager('ajii');
+  print(emplyee);
+
+  emplyee = VicePres('aaaaajiii');
+  print(emplyee);
+
+  //TODO Method Polymorph
+  sayHello(Emplyee('aajii'));
+  sayHello(Manager('aajiiaa'));
+  sayHello(VicePres('aasdajii'));
+}
+
+class Emplyee {
+  String name;
+
+  Emplyee(this.name);
+}
+
+class Manager extends Emplyee {
+  Manager(String name) : super(name);
+}
+
+class VicePres extends Manager {
+  VicePres(String name) : super(name);
+}
+
+//TODO Method Polymorph
+void sayHello(Emplyee emplyee) {
+  print('hello ${emplyee.name}');
+}
