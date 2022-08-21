cfa5db8dd7800195b5908d4512f062f930775727
aprxty3 <ajiprxty3@gmail.com>
2022-08-21 13:28:53 +0700

    ekse

--- lib/2.oop/13.Named_Parameter/example.dart
+++ lib/2.oop/13.Named_Parameter/example.dart
@@ -1,1 +1,26 @@
-void main(){}
+void main() {
+  var person = Person('aji', 'yogya');
+  var person1 = Person.withName('aji pras');
+  var person2 = Person.withAdress('Slawi');
+
+  print(person.name);
+  print(person.address);
+  print('--------------');
+  print(person1.name);
+  print(person1.address);
+  print('--------------');
+  print(person2.name);
+  print(person2.address);
+}
+
+class Person {
+  String name = 'Guest';
+  String? address;
+  final String country = "Indonesia";
+
+  Person(this.name, this.address);
+
+  Person.withName(this.name);
+
+  Person.withAdress(this.address);
+}
