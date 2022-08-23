cbefdd9466fde7405153b35e5a1e4f50dfd7d422
aprxty3 <ajiprxty3@gmail.com>
2022-08-23 23:00:58 +0700

    ekse

--- lib/2.oop/38.no_such_method/example.dart
+++ lib/2.oop/38.no_such_method/example.dart
@@ -1,1 +1,9 @@
-void main() {}
+import 'package:relearn_dart_with_pzn/2.oop/38.no_such_method/data.dart';
+
+void main() {
+  var repo = Repository("product");
+
+  repo.id('12324');
+  repo.name('laptop');
+  repo.quantity(1000);
+}
