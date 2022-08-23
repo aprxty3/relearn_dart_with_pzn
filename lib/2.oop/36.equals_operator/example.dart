45b6ed974a24a909ee4410a004e7ec13bf1ea540
aprxty3 <ajiprxty3@gmail.com>
2022-08-23 22:31:05 +0700

    ekse

--- lib/2.oop/36.equals_operator/example.dart
+++ lib/2.oop/36.equals_operator/example.dart
@@ -1,1 +1,10 @@
-void main() {}
+import 'package:relearn_dart_with_pzn/2.oop/27.import/data.dart';
+
+void main() {
+  var cate1 = Category('laptop', '1');
+  var cate2 = Category('laptop', '1');
+
+  print(cate1 ==
+      cate2); //false karna object yg berbeda akan berubah true jika override datanya
+  print(cate1 == cate1); //true karna objectnya sama
+}
