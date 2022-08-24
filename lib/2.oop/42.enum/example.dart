a2eb55c7f4becfa3b8b78720e13cfca5212ae2c8
aprxty3 <ajiprxty3@gmail.com>
2022-08-24 20:18:13 +0700

    ekse

--- lib/2.oop/42.enum/example.dart
+++ lib/2.oop/42.enum/example.dart
@@ -1,1 +1,16 @@
-void main() {}
+enum CustomerLevel { regular, premium, vip }
+
+class Customer {
+  String name;
+  CustomerLevel level;
+
+  Customer(this.name, this.level);
+}
+
+void main() {
+  var customer = Customer('ajii', CustomerLevel.premium);
+
+  print(customer.name);
+  print(customer.level);
+  print(CustomerLevel.values);
+}
