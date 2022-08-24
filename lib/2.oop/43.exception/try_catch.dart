b3108191f5eb6846b7ba5afadacc3a79ba66c20c
aprxty3 <ajiprxty3@gmail.com>
2022-08-24 20:29:50 +0700

    ekse

--- lib/2.oop/43.exception/try_catch.dart
+++ lib/2.oop/43.exception/try_catch.dart
@@ -3,7 +3,7 @@ import 'package:relearn_dart_with_pzn/2.oop/43.exception/data.dart';
 void main() {
   try {
     Validation.validate('', '');
-  } on ValidationException {
-    print('Validation Error');
+  } on ValidationException catch (exception) {
+    print('Error : ${exception.message}');
   }
 }
