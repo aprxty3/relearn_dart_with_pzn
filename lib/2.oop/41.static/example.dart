f11702ddad02dc16974f27e06fedcc33f3675212
aprxty3 <ajiprxty3@gmail.com>
2022-08-24 20:11:23 +0700

    ekse

--- lib/2.oop/41.static/example.dart
+++ lib/2.oop/41.static/example.dart
@@ -1,4 +1,8 @@
 void main() {
+  Application.oomm =
+      'wahyu'; //TODO: jika static tanpa final maka data bisa diubah
+
+  print(Application.oomm);
   print(Application.author);
   print(Application.name);
 }
@@ -6,4 +10,5 @@ void main() {
 class Application {
   static final String author = "aji praassss";
   static final String name = "belajar darrttt";
+  static String oomm = 'cippto';
 }
