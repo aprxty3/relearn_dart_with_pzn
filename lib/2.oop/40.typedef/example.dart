198ff8de19a46bee27f0843850a023f696bd7265
aprxty3 <ajiprxty3@gmail.com>
2022-08-24 19:36:10 +0700

    ekse

--- lib/2.oop/40.typedef/example.dart
+++ lib/2.oop/40.typedef/example.dart
@@ -1,1 +1,21 @@
-void main() {}
+void main() {
+  var jumlah = Jumlah(10, 10);
+  var total = Total(10, 10);
+
+  print(jumlah());
+  print(total());
+}
+
+class Sum {
+  int first;
+  int second;
+
+  Sum(this.first, this.second);
+
+  int call() {
+    return first + second;
+  }
+}
+
+typedef Jumlah = Sum;
+typedef Total = Sum;
