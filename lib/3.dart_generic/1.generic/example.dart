2bb6f55d73d69ca0dd71751fa3ae9e88671df649
aprxty3 <ajiprxty3@gmail.com>
2022-08-24 21:28:37 +0700

    ekse

--- lib/3.dart_generic/1.generic/example.dart
+++ lib/3.dart_generic/1.generic/example.dart
@@ -1,3 +1,10 @@
 //TODO bukan generic
+class Data {
+  dynamic data;
+}
 
-void main() {}
+void main(List<String> argument) {
+  var data = Data();
+  data.data = 'aji pras';
+  print(data.data);
+}
--- lib/3.dart_generic/1.generic/exmaple.dart
+++ lib/3.dart_generic/1.generic/exmaple.dart
@@ -1,1 +1,11 @@
 //TODO generic
+
+class Data<T> {
+  T? data;
+}
+
+void main(List<String> argument) {
+  var data = Data<String>();
+  data.data = 'ajiii pras';
+  print(data.data);
+}
