4e2519959c8d02f3da1fbaddcf0d816904832981
aprxty3 <ajiprxty3@gmail.com>
2022-08-24 19:30:20 +0700

    ekse

--- lib/2.oop/39.callable_class/example.dart
+++ lib/2.oop/39.callable_class/example.dart
@@ -1,1 +1,16 @@
-void main() {}
+void main() {
+  var sum = Sum(10, 10);
+
+  print(sum());
+}
+
+class Sum {
+  int first;
+  int second;
+
+  Sum(this.first, this.second);
+
+  int call() {
+    return first + second;
+  }
+}
