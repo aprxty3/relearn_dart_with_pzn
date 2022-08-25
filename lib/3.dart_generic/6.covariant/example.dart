43442c2b352bb65d8a9b39e1a4dde4730ec667c7
aprxty3 <ajiprxty3@gmail.com>
2022-08-25 22:42:01 +0700

    ekse

--- lib/3.dart_generic/6.covariant/example.dart
+++ lib/3.dart_generic/6.covariant/example.dart
@@ -1,1 +1,9 @@
-void main() {}
+import 'package:relearn_dart_with_pzn/3.dart_generic/2.generic_class/example.dart';
+
+void main() {
+  MyData<Object> data = new MyData<String>('Aji');
+
+  print(data.data);
+
+  data.data = 100; //error ketika berjlaan
+}
