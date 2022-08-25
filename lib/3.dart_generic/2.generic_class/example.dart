2b27c1700bcd40abd9856a6be3b0cd523e87623b
aprxty3 <ajiprxty3@gmail.com>
2022-08-25 22:09:07 +0700

    ekse

--- lib/3.dart_generic/2.generic_class/example.dart
+++ lib/3.dart_generic/2.generic_class/example.dart
@@ -1,1 +1,21 @@
-void main() {}
+void main() {
+  var dataString = MyData<String>('Aji');
+  var dataNumber = MyData(100);
+  var dataBool = MyData(true);
+
+  print(dataString.data);
+  print(dataNumber.data);
+  print(dataBool.data);
+}
+
+class MyData<T> {
+  T data;
+
+  MyData(this.data);
+
+  void test(T data) {}
+
+  T getData() {
+    return data;
+  }
+}
