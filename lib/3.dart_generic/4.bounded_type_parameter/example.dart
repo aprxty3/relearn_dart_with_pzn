45f034b50091be1dc6f05da3f787ed8eb5c76c47
aprxty3 <ajiprxty3@gmail.com>
2022-08-25 22:31:45 +0700

    ekse

--- lib/3.dart_generic/4.bounded_type_parameter/example.dart
+++ lib/3.dart_generic/4.bounded_type_parameter/example.dart
@@ -1,1 +1,18 @@
-void main() {}
+void main() {
+  //TODO Error karena data berupa String
+  // var dataString = NumberData('aji');
+  // var dataStringg = NumberData<String>('aaajii');
+
+  //TODO Success karena data berupa turunan dari num
+  var dataInt = NumberData(100);
+  var dataDouble = NumberData(0.4);
+
+  print(dataInt.data);
+  print(dataDouble.data);
+}
+
+class NumberData<T extends num> {
+  T data;
+
+  NumberData(this.data);
+}
