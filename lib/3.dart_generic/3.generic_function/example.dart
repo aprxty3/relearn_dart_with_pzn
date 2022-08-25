47fd9a8afd14a8d3d59050b1fbd6addefc884458
aprxty3 <ajiprxty3@gmail.com>
2022-08-25 22:19:43 +0700

    ekse

--- lib/3.dart_generic/3.generic_function/example.dart
+++ lib/3.dart_generic/3.generic_function/example.dart
@@ -1,1 +1,13 @@
-void main() {}
+void main() {
+  var numbers = [1, 1, 2, 4, 5, 6, 7];
+  var names = ['aji', 'pras', 'tyo'];
+
+  print(ArrayHelper.count(numbers));
+  print(ArrayHelper.count(names));
+}
+
+class ArrayHelper {
+  static int count<T>(List<T> list) {
+    return list.length;
+  }
+}
