2945df0beeb225aade097c737c330cb1d5c9158c
aprxty3 <ajiprxty3@gmail.com>
2022-08-25 22:47:27 +0700

    ekse

--- lib/3.dart_generic/7.type_checking/example.dart
+++ lib/3.dart_generic/7.type_checking/example.dart
@@ -1,1 +1,23 @@
-void main() {}
+import 'package:relearn_dart_with_pzn/3.dart_generic/2.generic_class/example.dart';
+
+void main() {
+  check(MyData('aajii'));
+  check(MyData(10));
+  check(MyData(false));
+
+  check('100');
+  check(100);
+  check(true);
+}
+
+void check(dynamic data) {
+  if (data is MyData<String>) {
+    print('String');
+  } else if (data is MyData<num>) {
+    print('num');
+  } else if (data is MyData<bool>) {
+    print('bool');
+  } else {
+    print('other');
+  }
+}
