46dab8d50f439ca20e5f32044a324e4ac7809fae
aprxty3 <ajiprxty3@gmail.com>
2022-08-25 22:36:28 +0700

    ekse

--- lib/3.dart_generic/5.dynamic/example.dart
+++ lib/3.dart_generic/5.dynamic/example.dart
@@ -1,1 +1,11 @@
-void main() {}
+import '../2.generic_class/example.dart';
+
+void printData(MyData data) {
+  print(data.data);
+}
+
+void main() {
+  printData(MyData('aaajiii'));
+  printData(MyData(123));
+  printData(MyData(true));
+}
