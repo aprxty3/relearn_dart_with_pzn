74da1f23d19d9d345c997467ee5d2fbb602f581a
aprxty3 <ajiprxty3@gmail.com>
2022-08-30 23:12:08 +0700

    ekse

--- lib/5.dart_collection/2.list/example.dart
+++ lib/5.dart_collection/2.list/example.dart
@@ -15,4 +15,11 @@ void main() {
   print(list1);
 
   // list1.add(10); //akan error karna datanya fixed dan gabisa bertambah
+  //mengubah data list
+  list1[0] = 1;
+  list1[1] = 19;
+  list1[2] = 21;
+  list1[3] = 42;
+  print('-----------');
+  print(list1);
 }
