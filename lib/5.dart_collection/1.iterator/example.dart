35748c2ea8ee73aa790cc9d5fbb761cab40e54ff
aprxty3 <ajiprxty3@gmail.com>
2022-08-30 21:15:38 +0700

    ekse

--- lib/5.dart_collection/1.iterator/example.dart
+++ lib/5.dart_collection/1.iterator/example.dart
@@ -1,7 +1,26 @@
 void main() {
-  var names = ['seth', 'logan', 'jium'];
+  final names = ['seth', 'logan', 'jium'];
+  final iterator = names.iterator;
 
+  //TODO : otomatis
   for (var name in names) {
     print(name);
   }
+
+  print('------------------');
+
+  while (iterator.moveNext()) {
+    print(iterator.current);
+  }
+
+  print('------------------');
+  //TODO : manual
+  iterator.moveNext();
+  print(iterator.current);
+
+  iterator.moveNext();
+  print(iterator.current);
+
+  iterator.moveNext();
+  print(iterator.current);
 }
