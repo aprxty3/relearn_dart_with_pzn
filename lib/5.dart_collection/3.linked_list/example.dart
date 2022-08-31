58cc16f22969f70bccd236b680af71b636a18e64
aprxty3 <ajiprxty3@gmail.com>
2022-08-31 22:54:55 +0700

    ekse

--- lib/5.dart_collection/3.linked_list/example.dart
+++ lib/5.dart_collection/3.linked_list/example.dart
@@ -1,1 +1,17 @@
-void main() {}
+import 'dart:collection';
+
+void main() {
+  var linkList = LinkedList<LinkList>();
+  linkList.add(LinkList('aji'));
+  linkList.add(LinkList('pras'));
+
+  for (var entry in linkList) {
+    print(entry.value);
+  }
+}
+
+class LinkList extends LinkedListEntry<LinkList> {
+  String value;
+
+  LinkList(this.value);
+}
