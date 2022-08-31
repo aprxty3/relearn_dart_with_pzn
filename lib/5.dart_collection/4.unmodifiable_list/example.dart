ec368715a162cb4dac82ee5562b5a9441ca493ca
aprxty3 <ajiprxty3@gmail.com>
2022-08-31 23:00:43 +0700

    ekse

--- lib/5.dart_collection/4.unmodifiable_list/example.dart
+++ lib/5.dart_collection/4.unmodifiable_list/example.dart
@@ -1,3 +1,8 @@
-//unmodifiable list == fix list
+//TODO : unmodifiable list == fix list
 
-void main() {}
+void main() {
+  final numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+  final unmodifList = List.unmodifiable(numbers);
+
+  unmodifList.add(12); //akan error karena unmodif/fix list
+}
