1349da96b49e2c3726615e27b9b70b80c195add2
aprxty3 <ajiprxty3@gmail.com>
2022-08-31 23:22:16 +0700

    ekse

--- lib/5.dart_collection/8.comporable/example.dart
+++ lib/5.dart_collection/8.comporable/example.dart
@@ -1,5 +1,20 @@
+import 'dart:collection';
+
 void main() {
   print(1.compareTo(2));
   print(1.compareTo(0));
   print(1.compareTo(1));
+  print('--------------');
+
+  final treeSet = SplayTreeSet<Category>();
+  treeSet.add(Category('1', 'Cate 1'));
+  treeSet.add(Category('2', 'Cate 2'));
+  treeSet.add(Category('3', 'Cate 3'));
+}
+
+class Category {
+  String id;
+  String name;
+
+  Category(this.id, this.name);
 }
