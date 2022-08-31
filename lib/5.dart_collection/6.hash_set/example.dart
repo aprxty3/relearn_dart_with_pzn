fd1c4faa26b972a8c1d549a42b741ab31728b3c3
aprxty3 <ajiprxty3@gmail.com>
2022-08-31 23:10:14 +0700

    ekse

--- lib/5.dart_collection/6.hash_set/example.dart
+++ lib/5.dart_collection/6.hash_set/example.dart
@@ -1,1 +1,15 @@
-void main() {}
+import 'dart:collection';
+
+void main() {
+  final set = HashSet<String>();
+
+  set
+    ..add('aji')
+    ..add('pras')
+    ..add('ok');
+
+  print(set);
+  print('aji'.hashCode);
+  print('pra'.hashCode);
+  print('ok'.hashCode);
+}
