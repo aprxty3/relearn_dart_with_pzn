8662b0ea2a212b7dac658c16287af33a3a0de951
aprxty3 <ajiprxty3@gmail.com>
2022-08-31 23:25:54 +0700

    ekse

--- lib/5.dart_collection/8.comporable/example.dart
+++ lib/5.dart_collection/8.comporable/example.dart
@@ -5,16 +5,27 @@ void main() {
   print(1.compareTo(0));
   print(1.compareTo(1));
   print('--------------');
-
+  //TODO error karena uncomparable
   final treeSet = SplayTreeSet<Category>();
   treeSet.add(Category('1', 'Cate 1'));
   treeSet.add(Category('2', 'Cate 2'));
   treeSet.add(Category('3', 'Cate 3'));
 }
 
-class Category {
+//TODO: harus implement Comparable agar type datanya masuk ke TreeSet
+class Category implements Comparable<Category> {
   String id;
   String name;
 
   Category(this.id, this.name);
+
+  @override
+  int compareTo(Category other) {
+    return id.compareTo(other.id);
+  }
+
+  @override
+  String toString() {
+    return 'Category{id: $id, name: $name}';
+  }
 }
