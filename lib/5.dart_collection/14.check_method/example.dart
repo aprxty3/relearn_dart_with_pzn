24e788b234b7378cb7ccf88c8864a2710a55ee47
aprxty3 <ajiprxty3@gmail.com>
2022-09-02 08:01:25 +0700

    ekse

--- lib/5.dart_collection/14.check_method/example.dart
+++ lib/5.dart_collection/14.check_method/example.dart
@@ -1,1 +1,8 @@
-void main() {}
+void main() {
+  final numbers = [2, 4, 5, 7, 8, 9, 12];
+
+  print(numbers
+      .any((element) => element > 5)); //true karena ada nilai yg lebih dari 5
+  print(numbers.every(
+      (element) => element > 5)); //false karena ada nilai yg kurang dari 5
+}
