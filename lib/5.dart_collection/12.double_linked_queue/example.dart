9676b4c3bb20569c306e3c92faade96346a2cb43
aprxty3 <ajiprxty3@gmail.com>
2022-09-02 07:54:57 +0700

    ekse

--- lib/5.dart_collection/12.double_linked_queue/example.dart
+++ lib/5.dart_collection/12.double_linked_queue/example.dart
@@ -1,1 +1,17 @@
-void main() {}
+import 'dart:collection';
+
+void main() {
+  final queue = DoubleLinkedQueue<String>();
+
+  queue.addLast('Ajii');
+  queue.addLast('Tyooo');
+  queue.addLast('Praasss');
+
+  print(queue);
+  print(queue.removeFirst());
+  print(queue);
+  print(queue.removeFirst());
+  print(queue);
+  print(queue.removeFirst());
+  print(queue);
+}
