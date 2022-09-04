5177671758b4f5e79bbafb9676bdf631368d8cc1
aprxty3 <ajiprxty3@gmail.com>
2022-09-04 21:34:45 +0700

    ekse

--- lib/5.dart_collection/15.filter_method/example.dart
+++ lib/5.dart_collection/15.filter_method/example.dart
@@ -1,1 +1,12 @@
-void main() {}
+void main() {
+  final numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  print(numbers.firstWhere((element) => element % 3 == 0));
+  print(numbers.lastWhere((element) => element % 3 == 0));
+  print(numbers.singleWhere(
+      (element) => element % 7 == 0)); //hanya bisa dibaginya hanya 1
+
+  print(numbers.skip(4));
+  print(numbers.take(4)); //mengambil data diawal
+
+}
