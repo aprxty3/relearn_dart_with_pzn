6b9d590aeed324b1a6a5ece342348c16bc03cde6
aprxty3 <ajiprxty3@gmail.com>
2022-09-04 22:50:00 +0700

    ekse

--- lib/5.dart_collection/24.linked_hash_map/example.dart
+++ lib/5.dart_collection/24.linked_hash_map/example.dart
@@ -1,1 +1,11 @@
-void main() {}
+import 'dart:collection';
+
+void main() {
+  final scores = LinkedHashMap<String, int>();
+
+  scores['aji'] = 100;
+  scores['pras'] = 100;
+  scores['setyo'] = 100;
+
+  print(scores);
+}
