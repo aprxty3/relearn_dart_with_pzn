2f9918a290b32f9ed7138788eecb205ce79c1765
aprxty3 <ajiprxty3@gmail.com>
2022-09-04 22:06:39 +0700

    ekse

--- lib/5.dart_collection/18.iterable_properties/example.dart
+++ lib/5.dart_collection/18.iterable_properties/example.dart
@@ -1,1 +1,9 @@
-void main() {}
+void main() {
+  final names = ['aji', 'pras', 'setyp'];
+
+  print(names.length);
+  print(names.first);
+  print(names.last);
+  print(names.isEmpty);
+  print(names.isNotEmpty);
+}
