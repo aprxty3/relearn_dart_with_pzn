432d2b561c287b140b334e5ed24f88e1c04f3a72
aprxty3 <ajiprxty3@gmail.com>
2022-09-04 22:13:20 +0700

    ekse

--- lib/5.dart_collection/19.list_method/example.dart
+++ lib/5.dart_collection/19.list_method/example.dart
@@ -1,7 +1,12 @@
 void main() {
   final names = ['aji', 'pras', 'typ', 'setuo'];
-  print(names);
+  final auther = ['masin', 'newbie', 'pak'];
 
+  final combine = names + auther;
+  print(combine);
+
+  print(names);
+  print(auther);
   names.insert(1, 'dwi');
   print(names);
 }
