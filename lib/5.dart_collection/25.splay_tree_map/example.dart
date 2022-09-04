469eaf8a21e27dbde57b4a3e5354e42848e1a595
aprxty3 <ajiprxty3@gmail.com>
2022-09-04 22:52:32 +0700

    ekse

--- lib/5.dart_collection/25.splay_tree_map/example.dart
+++ lib/5.dart_collection/25.splay_tree_map/example.dart
@@ -1,1 +1,12 @@
-void main() {}
+import 'dart:collection';
+
+void main() {
+  final scores = SplayTreeMap<String, int>();
+
+  scores['aji'] = 100;
+  scores['pras'] = 100;
+  scores['setyo'] = 100;
+  scores['dwi'] = 100;
+
+  print(scores);
+}
