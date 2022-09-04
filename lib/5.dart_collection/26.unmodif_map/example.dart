4e043e3f2e2a2d3bbb3629a9d87e967d86b13078
aprxty3 <ajiprxty3@gmail.com>
2022-09-04 22:55:45 +0700

    ekse

--- lib/5.dart_collection/26.unmodif_map/example.dart
+++ lib/5.dart_collection/26.unmodif_map/example.dart
@@ -1,1 +1,14 @@
-void main() {}
+void main() {
+  final Map<String, String> person = {
+    'firesName': 'Aji',
+    'lastName': 'Prasetyo'
+  };
+
+  final finalPerson = Map.unmodifiable(person);
+
+  print(person);
+  print(finalPerson);
+
+  finalPerson['middleName'] = 'prasetyi';
+  print(finalPerson);
+}
