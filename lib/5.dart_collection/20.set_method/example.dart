247f8a0016330e070725a7dc5507532b10a1e3ee
aprxty3 <ajiprxty3@gmail.com>
2022-09-04 22:16:35 +0700

    ekse

--- lib/5.dart_collection/20.set_method/example.dart
+++ lib/5.dart_collection/20.set_method/example.dart
@@ -1,1 +1,8 @@
-void main() {}
+void main() {
+  final names1 = {'aji', 'pras', 'tyo'};
+  final names2 = {'aji', 'setyo', 'dwi'};
+
+  print(names1.union(names2));
+  print(names1.intersection(names2));
+  print(names1.difference(names2));
+}
