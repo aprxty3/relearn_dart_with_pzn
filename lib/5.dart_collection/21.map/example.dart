fa7addec5e09b113edf410b5b54806939fdcbf73
aprxty3 <ajiprxty3@gmail.com>
2022-09-04 22:28:29 +0700

    ekse

--- lib/5.dart_collection/21.map/example.dart
+++ lib/5.dart_collection/21.map/example.dart
@@ -1,1 +1,12 @@
-void main() {}
+void main() {
+  final Map<String, String> person = {
+    'firesName': 'Aji',
+    'lastName': 'Prasetyo'
+  };
+
+  print(person);
+  print(person['firesName']);
+
+  person['middleName'] = 'Dwi';
+  print(person);
+}
