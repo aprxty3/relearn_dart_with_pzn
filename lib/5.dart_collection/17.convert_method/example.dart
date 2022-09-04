2a72a60a9759168ea4ee5125a56d8f7febfec357
aprxty3 <ajiprxty3@gmail.com>
2022-09-04 22:04:18 +0700

    ekse

--- lib/5.dart_collection/17.convert_method/example.dart
+++ lib/5.dart_collection/17.convert_method/example.dart
@@ -1,1 +1,12 @@
-void main() {}
+void main() {
+  final numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 4, 2, 5, 6];
+
+  final numberToSet = numbers.toSet();
+
+  final numberToList = numberToSet.toList(growable: true);
+  numberToList.add(10);
+
+  print(numbers);
+  print(numberToSet);
+  print(numberToList);
+}
