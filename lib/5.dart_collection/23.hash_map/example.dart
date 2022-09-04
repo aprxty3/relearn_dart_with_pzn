68d42bd8ae84118977d39bf3f20894cd60357cc2
aprxty3 <ajiprxty3@gmail.com>
2022-09-04 22:47:07 +0700

    ekse

--- lib/5.dart_collection/23.hash_map/example.dart
+++ lib/5.dart_collection/23.hash_map/example.dart
@@ -1,1 +1,11 @@
-void main() {}
+import 'dart:collection';
+
+void main() {
+  final scores = HashMap<String, int>();
+
+  scores['aji'] = 100;
+  scores['pras'] = 100;
+  scores['setyo'] = 100;
+
+  print(scores);
+}
