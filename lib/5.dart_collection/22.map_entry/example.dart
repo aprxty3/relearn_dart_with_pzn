4a71730b6181292ecdb53b61dd40e0672e146424
aprxty3 <ajiprxty3@gmail.com>
2022-09-04 22:43:49 +0700

    ekse

--- lib/5.dart_collection/22.map_entry/example.dart
+++ lib/5.dart_collection/22.map_entry/example.dart
@@ -1,1 +1,10 @@
-void main() {}
+void main() {
+  final Map<String, String> person = {
+    'firesName': 'Aji',
+    'lastName': 'Prasetyo'
+  };
+
+  for (var entry in person.entries) {
+    print('${entry.key} : ${entry.value}');
+  }
+}
